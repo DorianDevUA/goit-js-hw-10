8d9910ad2adf0a9cc30f1b4673b41735f5dd5b66
DorianDevUA <dorian.devua@gmail.com>
2024-04-15 12:48:50 +0400

    Adds css class visually-hidden

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import { fetchBreeds } from "./js/cat-api";
+import { fetchBreeds, fetchCatByBreed, renderBreedsList, renderCatInfo } from "./js/cat-api";
 
 const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loaderMsg = document.querySelector('.loader');
+const errorMsg = document.querySelector('.error');
+
+breedSelect.addEventListener('change', onChangeSelect);
 
 fetchBreeds()
   .then(breeds => {
+    loaderMsg.classList.add('visually-hidden');
+    
     const breedsList = createListElements(breeds);
     renderBreedsList(breedSelect, breedsList);
   })
-  .catch(error => console.log)
+  .catch(error => console.log(error));
 
 function createListElements(arr) {
   return arr
@@ -15,6 +22,39 @@ function createListElements(arr) {
     .join('');
 }
 
-function renderBreedsList(selector, markup) {
-  selector.insertAdjacentHTML('beforeend', markup)
+// function renderBreedsList(selector, markup) {
+//   selector.insertAdjacentHTML('beforeend', markup)
+// }
+
+function onChangeSelect(evt) {
+  loaderMsg.classList.remove('visually-hidden');
+  const selectedBreed = evt.currentTarget.value;
+  console.log(selectedBreed);
+
+  fetchCatByBreed(selectedBreed)
+    .then(response => {
+      response.map(cat => {
+        loaderMsg.classList.add('visually-hidden');
+        const markup = createMurkup(cat);
+        renderCatInfo(catInfo, markup);
+      });
+    })
+    .catch(error => console.log(error));
 }
+
+function createMurkup({ url, breeds }) {
+  const markup = breeds.map(({ name, description, temperament }) => {
+    return `<img src="${url}" alt="${name}" width=300>
+      <div>
+      <h2>${name}</h2>
+      <p>${description}</p>
+      <p><b>Temperament:</b> ${temperament}</p>
+      </div>`;
+  });
+
+  return markup;
+}
+
+// function renderCatInfo(selector, markup) {
+//   selector.innerHTML = markup;
+// }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,10 +1,11 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_lPe764UypqgPCYsOgInObmavNiawcZTZvUxTvDlSwt3esNPOiLbguAl2wNJJ9lsF';
 
 function fetchBreeds() {
   return fetch(`${BASE_URL}/breeds`)
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.statusText)
+        throw new Error(response.statusText);
       }
 
       return response.json();
@@ -12,7 +13,23 @@ function fetchBreeds() {
 }
 
 function fetchCatByBreed(breedId) {
+  return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
 
+      return response.json();
+    }
+  );
 }
 
-export { fetchBreeds, fetchCatByBreed };
+function renderBreedsList(selector, markup) {
+  selector.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderCatInfo(selector, markup) {
+  selector.innerHTML = markup;
+}
+
+export { fetchBreeds, fetchCatByBreed, renderBreedsList, renderCatInfo };
